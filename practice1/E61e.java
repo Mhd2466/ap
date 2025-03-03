8868fdf34773a8e4826e36a0928cc204f33931df
mohammadreza <mhd.doosti84@gmail.com>
2025-03-03 16:39:46 +0330

    E61e

---
+++ practice1/E61e.java
@@ -0,0 +1,32 @@
+package ap.practice1;
+
+import java.util.Scanner;
+
+public class E61e {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        int sum = 0;
+
+
+        while (num > 0) {
+            int digit = num % 10; 
+
+            if (digit % 2 != 0) { 
+                sum += digit;
+            }
+
+            num /= 10;
+        }
+
+
+        System.out.println("Sum of all odd digits: " + sum);
+
+
+    }
+    
+}
