bc08f7de6c793f7ab17b34553e36c0764c84a997
mohammadreza <mhd.doosti84@gmail.com>
2025-03-03 15:51:22 +0330

    E5.2

---
+++ practice1/E52.java
@@ -0,0 +1,31 @@
+package ap.practice1;
+import java.util.Scanner;
+public class E52 {
+    public static void main(String[] args) {
+      Scanner myObj = new Scanner(System.in);
+      
+
+      System.out.println("Give me a number:"); 
+      float myNum = myObj.nextFloat(); 
+      if (myNum==0) {
+        System.out.println("zero");
+        
+      }
+      else if (myNum>0) {
+        System.out.println("positive");
+        
+      }
+      else if (myNum<0) {
+        System.out.println("negative");
+      }
+      if (myNum<1) {
+        System.out.println("small");
+        
+      }
+      if (myNum>1000000) {
+        System.out.println("large");
+        
+      }
+      
+    }
+  }
