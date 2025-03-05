57458005537e50faad8a3525e88a7089948949d3
mohammadreza <mhd.doosti84@gmail.com>
2025-03-06 01:52:52 +0330

    E6.16

---
+++ practice1/E616.java
@@ -0,0 +1,14 @@
+package ap.practice1;
+
+public class E616 {
+    public static void main(String[] args) {
+        int size = 10; 
+
+        for (int i = 1; i <= size; i++) {
+            for (int j =1; j <=size; j++) {
+                System.out.print(i *j + "\t"); // Print with taab
+            }
+            System.out.println();
+        }
+    }
+}
