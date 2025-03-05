76a4a20f8ea24acce8db446f00a0c43630bf297a
mohammadreza <mhd.doosti84@gmail.com>
2025-03-06 01:52:38 +0330

    E6.13

---
+++ practice1/E613.java
@@ -0,0 +1,23 @@
+package ap.practice1;
+
+import java.util.Scanner;
+
+public class E613 {
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (number == 0) {
+            System.out.println(0);
+        } else {
+            while (number > 0) {
+                System.out.println(number % 2); 
+                number /= 2;
+            }
+        }
+        
+        scanner.close();
+    }
+}
