de26b6fafe0307eddd67294573279d388ac2908d
mohammadreza <mhd.doosti84@gmail.com>
2025-03-05 12:21:14 +0330

    E6.8

---
+++ practice1/E68.java
@@ -0,0 +1,20 @@
+package ap.practice1;
+
+import java.util.Scanner;
+
+public class E68 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a word: ");  
+         
+        String word = scanner.nextLine();
+        
+        for (int i = 0; i < word.length(); i++) {
+            
+            System.out.println(word.charAt(i));
+        }
+        
+        scanner.close();
+    }
+}
