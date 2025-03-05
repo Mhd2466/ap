b593c6141c76786dc9466730ee7df5ca80785709
mohammadreza <mhd.doosti84@gmail.com>
2025-03-06 02:09:50 +0330

    E6.17

---
+++ practice1/E617.java
@@ -0,0 +1,47 @@
+package ap.practice1;
+
+import java.util.Scanner;
+
+public class E617 {
+        public static void main(String[] args) {
+
+
+         Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("enter the length of the squares: ");
+
+        int sideLength = scanner.nextInt();
+
+
+
+
+
+
+
+        for (int i = 0; i < sideLength; i++) {
+
+            for (int j = 0; j < sideLength; j++) {
+                System.out.print("*");
+            }
+
+            System.out.print(" ");
+
+            for (int j = 0; j < sideLength; j++) {
+                if (i == 0 || i == sideLength - 1 || j == 0 || j == sideLength - 1) {
+                    System.out.print("*");
+                } else {
+
+
+                    System.out.print(" ");
+                }
+            }
+
+
+            System.out.println();
+        
+        scanner.close();
+    
+}
+        }
+    }
