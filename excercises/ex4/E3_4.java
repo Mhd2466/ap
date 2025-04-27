6e1b9c1bfc7f7c11443ccace22c334acb4205cc7
mohammadreza <mhd.doosti84@gmail.com>
2025-04-27 19:17:52 +0330

    E3.4

---
+++ excercises/ex4/E3_4.java
@@ -0,0 +1,36 @@
+package ap.excercises.ex4;
+
+public class E3_4 {
+    public class HallwayLightControl {
+        private boolean firstSwitchUp;
+        private boolean secondSwitchUp;
+    
+        public HallwayLightControl() {
+            
+            firstSwitchUp = false;
+            secondSwitchUp = false;
+        }
+    
+        public int getFirstSwitchState() {
+            return firstSwitchUp ? 1 : 0;
+        }
+    
+        public int getSecondSwitchState() {
+            return secondSwitchUp ? 1 : 0;
+        }
+    
+        public int getLampState() {
+            
+            return (firstSwitchUp ^ secondSwitchUp) ? 1 : 0;
+        }
+    
+        public void toggleFirstSwitch() {
+            firstSwitchUp = !firstSwitchUp;
+        }
+    
+        public void toggleSecondSwitch() {
+            secondSwitchUp = !secondSwitchUp;
+        }
+    }
+    
+}
