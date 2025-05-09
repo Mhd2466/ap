5b696f539a8ad30ef1a502ba49260e37ae1927ae
mohammadreza <mhd.doosti84@gmail.com>
2025-05-09 18:54:48 +0330

    EX4_P4_5

---
+++ excercises/ex4/Boat.java
@@ -0,0 +1,40 @@
+package ap.excercises.ex4;
+
+public class Boat {
+    private double x;
+    private double y;
+    private double direction; //   0 = east
+
+    public Boat() {
+        x = 0;
+        y = 0;
+        direction = 0;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public void turn(double degrees) {
+        direction += degrees;
+        direction = direction % 360; // keep it in 0, 360 :)
+        if (direction < 0) {
+            direction += 360;
+        }
+    }
+
+    public void move(double distance) {
+        double radians = Math.toRadians(direction);
+        x += distance * Math.cos(radians);
+        y += distance * Math.sin(radians);
+    }
+}
+
