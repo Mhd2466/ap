e2911a2f427763157a388d2abe2335f353b345b3
mohammadreza <mhd.doosti84@gmail.com>
2025-11-07 00:40:52 +0330

    E4_9

---
+++ exp04/E4_9.java
@@ -0,0 +1,23 @@
+package ap.exp04;
+
+import java.util.Scanner;
+
+public class E4_9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = input.nextLine();
+        StringBuilder result = new StringBuilder();
+        int count = 1;
+        for (int i = 1; i <= s.length(); i++) {
+            if (i < s.length() && s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                result.append(s.charAt(i - 1)).append(count);
+                count = 1;
+            }
+        }
+        System.out.println(result.toString());
+        input.close();
+    }
+}
